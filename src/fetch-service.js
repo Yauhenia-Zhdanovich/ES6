d1bbe6bba68d34e74050649dfe5d064a1a7b2038
evgesha0411 <zhdanovichevgenia@gmail.com>
2018-11-29 00:47:55 +0300

    webpack task implementation

--- src/fetch-service.js
+++ src/fetch-service.js
@@ -4,8 +4,20 @@ export class FetchService {
   fetchDataFromServer(newsChannelId, key) {
     return fetch(`https://newsapi.org/v1/articles?source=${newsChannelId}&apiKey=${key}`)
       .then(resp => resp.json())
+      .catch(err => {
+        console.log(err);
+        return [];
+      })
       .then(data => {
         return data.articles || [];
       });
   }
 }
+// {
+//   "presets" : ["es2015", "react"],
+// "plugins": [
+//       "transform-object-rest-spread",
+//       "@babel/plugin-proposal-object-rest-spread",
+//       "transform-runtime"
+//   ]
+// }
--- src/index.js
+++ src/index.js
@@ -1,84 +1,14 @@
-import './style.css';
-import { createNewsChannelItem } from "./news-channel-item";
-import { arrayOfNewsChannels } from './constants';
-import { createArtciles } from './create-articles';
-import { Loader } from "./components/loader";
+import './style.scss';
 
-let currentChannel = 'cnn';
-let loader;
+const main = () => {
+  const uploadButton = document.querySelector('.upload-button');
 
-const setCssClass = (currentValue, className) => {
-  let previousItem = document.querySelector(`.${className}`);
-  previousItem.classList.remove(`${className}`);
-  let currentItem = document.querySelector(`#${currentValue}`);
-  currentItem.classList.add(`${className}`);
-};
-
-const onContainerClick  = (event) => {
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  const newsChannelId = event.target.innerHTML;
-  setCssClass(newsChannelId, 'active-channel');
-
-  if (currentChannel !== newsChannelId) {
-    const newsContainer = document.querySelector('#newsChannels');
-    const news = document.querySelector('#news');
-    const newArticlesContainer = document.createElement('div');
-    const currentArticles = document.querySelector('.news-articles');
-
-    newsContainer.removeEventListener('click', onContainerClick);
-    news.removeChild(currentArticles);
-    newArticlesContainer.classList.add('news-articles');
-  
-    currentChannel = newsChannelId;
-    news.appendChild(newArticlesContainer);
-    loader.showLoader();
-    createArtciles(newsChannelId)
-      .then(data => {
-        Promise.all(data.arrayOfPromises).then(() => {
-          data.arrayOfArticles.forEach(element => {
-          newArticlesContainer.appendChild(element);
-          });
-          loader.hideLoader();
-          newsContainer.addEventListener('click', onContainerClick);
-        })
-      })
-      .catch(err => {
-        console.log(err);
-        newsContainer.addEventListener('click', onContainerClick);
-      });
-  }
-};
-
-let myFunc = () => {
-  const newsContainer = document.querySelector('#newsChannels');
-  const news = document.querySelector('#news');
-  const newsArticlesContainer = document.createElement('div');
-
-  loader = new Loader(news);
-  loader.createLoader();
-
-  newsArticlesContainer.classList.add('news-articles');
-  news.appendChild(newsArticlesContainer);
-  loader.showLoader();
-  createArtciles(currentChannel).then(data => {
-    Promise.all(data.arrayOfPromises)
-    .then(() => {
-      data.arrayOfArticles.forEach(element => {
-      newsArticlesContainer.appendChild(element);
-      });
-      loader.hideLoader();
-    })
-    .catch(err => console.log(err));
-  })
-  .then(() => {
-    newsContainer.addEventListener('click', onContainerClick);
+  uploadButton.addEventListener('click', () => {
+    import('./upload-content.js').then(module => {
+      let uploading = module.default;
+      uploading();
+    });
   });
+}
 
-  arrayOfNewsChannels.forEach(channel => {
-    newsContainer.appendChild(createNewsChannelItem(channel, currentChannel));
-  });
-};
-
-document.addEventListener('load', myFunc());
+document.addEventListener('load', main());
